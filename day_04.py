6748a5f399695f60028ff81409b1c00eae0233ff
Michael Erne <michael.erne@gmail.com>
2025-12-04 06:21:01 +0100

    day_04 -> k-core pruning

--- day_04.py
+++ day_04.py
@@ -1,87 +1,78 @@
-from run_util import run_puzzle
+from collections import deque
 from typing import List
+from run_util import run_puzzle
 
-def parse_data(data) -> List[List[str]]:
-    return [list(line.strip()) for line in data.strip().splitlines()]
+
+def parse_data(data: str) -> List[List[int]]:
+    data = data.strip()
+    lines = data.splitlines()
+    grid = [list(line.strip()) for line in lines]
+
+    roll_index_by_position: dict[tuple[int, int], int] = {}
+    roll_positions: List[tuple[int, int]] = []
+
+    for y, row in enumerate(grid):
+        for x, cell in enumerate(row):
+            if cell == '@':
+                index = len(roll_positions)
+                roll_positions.append((x, y))
+                roll_index_by_position[(x, y)] = index
+
+    roll_count = len(roll_positions)
+    neighbors: List[List[int]] = [[] for _ in range(roll_count)]
+
+    neighbor_deltas = [
+        (-1, -1), (-1, 0), (-1, 1),
+        (0, -1),            (0, 1),
+        (1, -1),   (1, 0),  (1, 1),
+    ]
+
+    for node_index, (x, y) in enumerate(roll_positions):
+        for d_x, d_y in neighbor_deltas:
+            n_x, n_y = x + d_x, y + d_y
+            neighbor_index = roll_index_by_position.get((n_x, n_y))
+            if neighbor_index is not None:
+                neighbors[node_index].append(neighbor_index)
+
+    return neighbors
 
 
 def part_a(data):
-    grid = parse_data(data)
-    row_count = len(grid)
-    column_count = len(grid[0])
-
-    accessible_count = 0
-
-    neighbor_deltas = [
-        (-1, -1), (-1, 0), (-1, 1),
-        (0, -1),           (0, 1),
-        (1, -1),  (1, 0),  (1, 1),
-    ]
-
-    for row_index in range(row_count):
-        for column_index in range(column_count):
-            if grid[row_index][column_index] != '@':
-                continue
-
-            neighbor_rolls = 0
-            for delta_row, delta_col in neighbor_deltas:
-                neighbor_row = row_index + delta_row
-                neighbor_col = column_index + delta_col
-                if (
-                    0 <= neighbor_row < row_count
-                    and 0 <= neighbor_col < len(grid[neighbor_row])
-                    and grid[neighbor_row][neighbor_col] == '@'
-                ):
-                    neighbor_rolls += 1
-
-            if neighbor_rolls < 4:
-                accessible_count += 1
-
-    return accessible_count
+    neighbors = parse_data(data)
+    return sum(1 for adj in neighbors if len(adj) < 4)
 
 
 def part_b(data):
-    grid = parse_data(data)
-    row_count = len(grid)
-    column_count = len(grid[0])
+    neighbors = parse_data(data)
+    node_count = len(neighbors)
 
-    neighbor_deltas = [
-        (-1, -1), (-1, 0), (-1, 1),
-        (0, -1),           (0, 1),
-        (1, -1),  (1, 0),  (1, 1),
-    ]
+    degrees = [len(adj) for adj in neighbors]
+    queue = deque()
+    in_queue = [False] * node_count
+    removed = [False] * node_count
+
+    for node_index, degree in enumerate(degrees):
+        if degree < 4:
+            queue.append(node_index)
+            in_queue[node_index] = True
 
     total_removed = 0
 
-    while True:
-        rolls_to_remove = []
+    while queue:
+        node = queue.popleft()
+        if removed[node]:
+            continue
 
-        for row_index in range(row_count):
-            for column_index in range(column_count):
-                if grid[row_index][column_index] != '@':
-                    continue
+        removed[node] = True
+        total_removed += 1
 
-                neighbor_rolls = 0
-                for delta_row, delta_col in neighbor_deltas:
-                    neighbor_row = row_index + delta_row
-                    neighbor_col = column_index + delta_col
-                    if (
-                        0 <= neighbor_row < row_count
-                        and 0 <= neighbor_col < len(grid[neighbor_row])
-                        and grid[neighbor_row][neighbor_col] == '@'
-                    ):
-                        neighbor_rolls += 1
-
-                if neighbor_rolls < 4:
-                    rolls_to_remove.append((row_index, column_index))
-
-        if not rolls_to_remove:
-            break
-
-        for row_index, column_index in rolls_to_remove:
-            grid[row_index][column_index] = '.'
-
-        total_removed += len(rolls_to_remove)
+        for neighbor in neighbors[node]:
+            if removed[neighbor]:
+                continue
+            degrees[neighbor] -= 1
+            if degrees[neighbor] < 4 and not in_queue[neighbor]:
+                in_queue[neighbor] = True
+                queue.append(neighbor)
 
     return total_removed
 
